6524b960e68a332abe7d230b0f81a2b67d76efe7
Cristian Garner <ceegarner@gmail.com>
2018-11-19 14:29:12 -0600

    Bank can enroll, deposit, withdraw

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,6 @@ var SimpleBank = artifacts.require("./SimpleBank.sol");
 //var Amazon = artifacts.require("./Amazon.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleBank);
+  deployer.deploy(SimpleBank, web3.eth.accounts[0]);
   //deployer.deploy(Amazon);
 };
